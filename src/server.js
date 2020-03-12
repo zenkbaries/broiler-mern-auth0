1a65002b1b4b341d5a23a85eb78a023e704a8047
John Towery <kbaries@gmail.com>
2020-03-11 20:55:38 -0600

    create server.js as api

---
+++ src/server.js
@@ -0,0 +1,43 @@
+// src/server.js
+
+const express = require("express");
+const cors = require("cors");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+
+// Create a new Express app
+const app = express();
+
+// Accept cross-origin requests from the frontend app
+app.use(cors({ origin: 'http://localhost:3000' }));
+
+// Set up Auth0 configuration
+const authConfig = {
+  domain: "dev-gviqn817.auth0.com",
+  audience: "https://boilerplateAPI.kellyboy.com"
+};
+
+// Define middleware that validates incoming bearer tokens
+// using JWKS from dev-gviqn817.auth0.com
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ["RS256"]
+});
+
+// Define an endpoint that must be called with an access token
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your Access Token was successfully validated!"
+  });
+});
+
+// Start the app
+app.listen(3001, () => console.log('API listening on 3001'));
