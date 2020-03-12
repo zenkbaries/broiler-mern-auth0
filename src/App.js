8e41da02dc07bf2f166d4b405f7007eda21e1b33
John Towery <kbaries@gmail.com>
2020-03-11 20:57:30 -0600

    import extAPI

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Router, Route, Switch } from "react-router-dom";
 import Profile from "./components/Profile";
 import history from "./utils/history";
 import PrivateRoute from "./components/PrivateRoute";
+import ExternalApi from "./views/ExternalApi";
 
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
         <Switch>
           <Route path="/" exact />
           <PrivateRoute path="/profile" component={Profile} />
+          <PrivateRoute path="/external-api" component={ExternalApi} />
         </Switch>
       </Router>        
     </div>
